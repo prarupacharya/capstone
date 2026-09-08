237e61bc2e938fc0a5a1decb9548119e8743da44
prarupacharya <prarupacharya@lftechnology.com>
2026-09-08 16:35:31 +0545

    Revert "feat(backend): configure JWT access tokens"

--- packages/backend/src/modules/auth/auth.module.ts
+++ packages/backend/src/modules/auth/auth.module.ts
@@ -1,26 +1,11 @@
 import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { parseJwtConfig } from "../../config/env";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      global: true,
-      useFactory: () => {
-        const config = parseJwtConfig();
-        return {
-          secret: config.secret,
-          signOptions: { expiresIn: config.expiresIn }
-        };
-      }
-    }),
-    UsersModule
-  ],
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [JwtModule]
+  providers: [AuthService]
 })
 export class AuthModule {}
